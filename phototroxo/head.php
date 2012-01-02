f1168c8cb2eae346054d56017a1e83de37e694bb
pulimento <pulimento@5c69fa66-d05c-0d97-aad1-aaddf8c542ee>
2012-01-02 18:23:26 +0000

    Se muestran las fotos recientes en el head.php ¡Probadlo! :p

--- phototroxo/head.php
+++ phototroxo/head.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$idU = $_SESSION["idUsuario"];
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
@@ -15,23 +16,50 @@ session_start();
 	<body>
 		<!-- Cabecera(logo y menú) -->
 		<?php
-			include ("cabecera.php");
+		include ("cabecera.php");
 		?>
 
 		<!-- Contenido -->
-		<div align="center">
-			<br />
-			<label><b>&Uacute;ltimas fotos subidas</b></label>
+		<div id="ultimasfotos">
+			<h2>
+				&Uacute;ltimas fotos subidas
+			</h2>
+			<?php
+			$link = mysql_connect("localhost", "root", "") or die ;
+			mysql_select_db("phototroxo", $link);
+			echo "idUsuario-> " . $idU . "<br/>";
+			$result = mysql_query("SELECT i.ruta, i.titulo, i.idI FROM imagen i WHERE idU = '$idU' ORDER BY idI DESC LIMIT 4", $link);
+			echo "<br/>la consulta devuelve " . mysql_num_rows($result) . " tupla(s)<br/>";
+			for ($i = 0; $i < mysql_num_rows($result); $i++) {
+				//Añade las fotos de la base de datos a un array
+				$arrayfotos[$i] = mysql_fetch_row($result);
+			}
+
+			// Ahora comprobaremos que todo ha ido correctamente (tratamiento de errores)
+			$my_error = mysql_error($link);
+
+			if (!empty($my_error)) {//Si hay error accediendo a la BD
+				echo "Ha habido un error accediendo a la base de datos. Inténtelo más tarde. $my_error";
+			} else {
+				//Mostrar las fotos
+				echo "<table id=\"tabla_ultimasfotos\"><tr>";//Inicio de la tabla
+				for ($j = 0; $j < mysql_num_rows($result); $j++) {
+					echo "<td class=\"celda_tabla_fotos\"><img class=\"foto_tabla\" src=\"" . $arrayfotos[$j][0] . "\"</img></td>";
+				}
+				echo "</tr></table>";//Cerramos las etiquetas de la tabla
+			}
+			?>
 		</div>
-		<div align="center">
-			<br />
-			<label><b>&Uacute;ltimos comentarios</b></label>
+		<div id="ultimoscomentarios">
+			<h2>
+				&Uacute;ltimos comentarios
+			</h2>
 		</div>
-		<div align="left">
-			<a href="quienessomos.html">¿Qui&eacute;nes somos?</a>
+		<div id="quienessomos">
+			<a href="quienessomos.html">&iquest;Qui&eacute;nes somos?</a>
 		</div>
-		<div id = "sugerencia">
-			<a href="mailto:staff@phototroxo.com">Enviar sugerencia al correo</a>
+		<div id="sugerencia">
+			<a href="mailto:staff@phototroxo.com">Env&iacute;anos una sugerencia</a>
 		</div>
 	</body>
 </html>
