f45f3e25f606d1420edf65232feb517c4d8210bc
pulimento <pulimento@5c69fa66-d05c-0d97-aad1-aaddf8c542ee>
2012-01-02 15:02:47 +0000

    Cambiado ligeramente el estilo de cabecera, ahora se puede cerrar sesión

--- trunk/phototroxo/cabecera.php
+++ trunk/phototroxo/cabecera.php
@@ -1,3 +1,17 @@
+<div id="cabecera">
+	<div id="sesion">
+	<?php
+	if (!empty($_SESSION["nombreUsuario"])) {
+		echo "Bienvenid@ " . $_SESSION["nombreUsuario"]."  ";
+		echo "<a href=\"cerrarsesion.php\">Cerrar sesi&oacute;n</a>";
+	} else {
+		//Cuando se está dentro de la página y no están declaradas las variables de sesión
+		//(no se ha iniciado sesión), ahora mismo muestra esto, pero debería mandar al index
+		//para que se inicie la sesión. Esto está así de prueba
+		echo "No se ha iniciado sesión!!!";
+	}
+	?>
+</div>
 <div id="div_header">
 	<img id="img_header" src="images/header.png" alt="Phototroxo" />
 </div>
@@ -9,3 +23,4 @@
 		<td class="celdamenu"><a href="subir_fotos.php">Sube una foto</a></td>
 	</tr>
 </table>
+</div>
--- trunk/phototroxo/cerrarsesion.php
+++ trunk/phototroxo/cerrarsesion.php
@@ -3,6 +3,6 @@ session_start();
 unset($_SESSION["nombre_usuario"]);
 unset($_SESSION["nombre_cliente"]);
 session_destroy();
-header("Location: index.php");
+header("Location: index.html");
 exit ;
 ?>
