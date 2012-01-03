5b037aad2a6dbc4660169b49c231d9426fdd4148
cristidelmarco@gmail.com <cristidelmarco@gmail.com@5c69fa66-d05c-0d97-aad1-aaddf8c542ee>
2012-01-03 15:57:43 +0000

    NuevaContraseña sin fallos. Cristina

--- phototroxo/nuevacontrasena.php
+++ phototroxo/nuevacontrasena.php
@@ -25,35 +25,36 @@
 		if (strlen($user) < 4) {
 				echo "- El usuario debe tener al menos cuatro caracteres<br/>";
 				$validado = false;
-			}
+		}
 		
 		if (strlen($password) < 6) {
 				echo "- La contraseña debe tener al menos seis caracteres<br/>";
 				$validado = false;
-			}
+		}
 		//fin de validacion de datos, conexion con la base de datos
 		
 		if ($validado) {
 				$link = mysql_connect("localhost", "root", "") or die ;
 				mysql_select_db("phototroxo", $link);
-		}
+		
 		
 		$result=mysql_query("SELECT * FROM usuario WHERE usser=$usser", $conexion);
 
-		$result=mysql_query("UPDATE usuario SET password=$password where usser=$usser,$conexion);
+		$result=mysql_query("UPDATE usuario SET password=$password where usser=$usser,$conexion");
 		
 		$my_error = mysql_error($link);
 
-				if (!empty($my_error)) {
-					echo "Ha habido un error al insertar los valores. $my_error";
-				} else {
-					echo "Te acabas de registrar satisfactoriamente en Phototroxo, " . $user . " ;) <br/><br/>";
-					echo "Puedes volver a la <a id=\"inicio\" href=\"index.html\">página principal</a> para iniciar sesión";
-				}
-			} else {// No se ha superado la validación del lado del servidor
-				echo "<br/><br/>Los datos que ha introducido no son válidos, por favor <a href=\"#\" onclick=\"history.back(1);return false\">vuelva a intentarlo</a>";
-			}
-		?>
+		if (!empty($my_error)) {
+			echo "Ha habido un error al insertar los valores. $my_error";
+		} else {
+			echo "Te acabas de registrar satisfactoriamente en Phototroxo, " . $user . " ;) <br/><br/>";
+			echo "Puedes volver a la <a id=\"inicio\" href=\"index.html\">página principal</a> para iniciar sesión";
+		}
+	}
+	else {// No se ha superado la validación del lado del servidor
+		echo "<br/><br/>Los datos que ha introducido no son válidos, por favor <a href=\"#\" onclick=\"history.back(1);return false\">vuelva a intentarlo</a>";
+	}
+?>
 		
 		
 		
