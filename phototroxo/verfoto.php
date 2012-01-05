14a3f4f1514e6fa0a531081600e3576011c16260
pulimento <pulimento@5c69fa66-d05c-0d97-aad1-aaddf8c542ee>
2012-01-05 14:45:37 +0000

    Añadido el sistema de comentarios a las fotos (verfotos.php) :D -- HAY QUE VOLVER A ACTUALIZAR LA BASE DE DATOS, faltaba un campo

--- phototroxo/verfoto.php
+++ phototroxo/verfoto.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
 $idU = $_SESSION["idUsuario"];
+$idFoto = $_GET["idI"];
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
 "http://www.w3.org/TR/html4/strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="es">
@@ -16,17 +18,15 @@ $idU = $_SESSION["idUsuario"];
 		<!-- Cabecera(logo y menú) -->
 		<?php
 		include ("cabecera.php");
-		?>
-
-		<!-- Contenido -->
-		<!-- <div id="div_content"> -->
+		?> <!-- Contenido -->
 		<?php
-		$idFoto = $_GET["idI"];
+
+		//Conectando a la base de datos
 		$link = mysql_connect("localhost", "root", "") or die ;
 		mysql_select_db("phototroxo", $link);
-		$result = mysql_query("SELECT i.ruta AS path, i.titulo, i.idI, u.Nombre AS nombreUploader, 
-			u.Apellidos AS apellidosUploader FROM (imagen AS i NATURAL JOIN usuario AS u) 
-			WHERE idU = '$idU' AND idI = '$idFoto'", $link);
+		$result = mysql_query("SELECT i.ruta AS path, i.titulo, i.idI, u.Nombre AS nombreUploader,
+		u.Apellidos AS apellidosUploader FROM (imagen AS i NATURAL JOIN usuario AS u)
+		WHERE idU = '$idU' AND idI = '$idFoto'", $link) or die ;
 		$foto = mysql_fetch_array($result);
 		$my_error = mysql_error($link);
 
@@ -54,11 +54,44 @@ $idU = $_SESSION["idUsuario"];
 		<div id="div_photo">
 			<img id="foto" <?php echo "src=\"" . $ruta . "\"></img>";?>
 			</div>
-			<div id="comentarios">
-			<!-- Hay que hacerlo -->
-			Aqui van los comentarios
+
+			<div id="escribircomentario">
+			<form method="post" action="<?php echo $_SERVER['PHP_SELF'] . "?idI=" . $idFoto;?>">
+			<label id="label_escribecomentario" for="input_escribecomentario">Escribe tu comentario: </label>
+			<input type="text" maxlength="160" id="input_escribecomentario" name="input_escribecomentario"></input>
+			<!-- <input type="hidden" name="input_user" value="<?php echo $idU; ?>"> -->
+			<input type="submit" value="submit" name="submit">
+			</form>
 			</div>
-			<!-- 				</div> -->
+			<?php
+			if(isset($_POST["input_escribecomentario"]))
+			$commentposted_comment = $_POST["input_escribecomentario"];
+			//$commentposted_user = $_POST("input_user");
+			if (isset($commentposted_comment)) {
+				$resultinsertarcomentario = mysql_query("INSERT INTO comentario (idI, idU, comentario)
+			VALUES ('$idFoto','$idU','" . $commentposted_comment . "')", $link);
+			}
+			//Falta el tratamiento de errores
+			?>
+
+			<div id="comentarios">
+			<?php
+			$resultcomentario = mysql_query("SELECT c.comentario AS comment, u.Nombre AS name
+			FROM (comentario AS c NATURAL JOIN usuario AS u) WHERE idI = '$idFoto'", $link) or die ;
+			$my_error = mysql_error($link);
+
+			if (!empty($my_error)) {//Si hay error accediendo a la BD
+				echo "Ha habido un error accediendo a la base de datos. Inténtelo más tarde. $my_error";
+			} else {
+				for ($cont = 0; $cont < mysql_num_rows($resultcomentario); $cont++) {
+					$comentario = mysql_fetch_array($resultcomentario);
+					$comentario_user = $comentario["name"];
+					$comentario_comentario = $comentario["comment"];
+					echo "user -> " . $comentario_user . "<br/>";
+					echo "comentario ->" . $comentario_comentario . "<br/>";
+				}
+			}
+			?>
+			</div>
 			</body>
 			</html>
-			5
