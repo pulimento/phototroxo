675baf808f54bc0228f04ad919ac094cadd82ca1
pulimento <pulimento@5c69fa66-d05c-0d97-aad1-aaddf8c542ee>
2012-01-05 15:58:17 +0000

    Añadida la cabecera al quienessomos, y renombrado a php

--- phototroxo/piedepagina.php
+++ phototroxo/piedepagina.php
@@ -1,6 +1,6 @@
 <div id="piedepagina">
 	<p id="text_piedepagina">
-		&copy; Phototroxo 2011/12 <a  id="quienessomos" href="quienessomos.html">&iquest;Qui&eacute;nes somos?</a>
+		&copy; Phototroxo 2011/12 <a  id="quienessomos" href="quienessomos.php">&iquest;Qui&eacute;nes somos?</a>
 		<a id="envianosunasugerencia" href="mailto:staff@phototroxo.com">Env&iacute;anos una sugerencia</a>
 	</p>
 </div>
