a6c6e9c317696257df9aaa78fcc4e3d4f7f22351
yamas2136@gmail.com <yamas2136@gmail.com@5c69fa66-d05c-0d97-aad1-aaddf8c542ee>
2012-01-09 17:28:44 +0000

    Hecho buscar fotos. Aunque falta como mostrar el resultado. No sé muy bien como ponerlo.

--- trunk/phototroxo/buscar_fotos.php
+++ trunk/phototroxo/buscar_fotos.php
@@ -17,10 +17,54 @@ session_start();
 		include ("cabecera.php");
 		?> <!-- Contenido -->
 		<div id="div_content">
-			Aquí lo que habría que poner es un formulario con un solo campo de búsqueda,</br>
-			hacer la consulta SQL correspondiente, y buscar por título de foto. Luego habría que</br>
-			mostrar los resultados en una lista
-		</div>
+			<form action="buscar.php" method="post">
+			Buscar foto: <input name="palabra">
+			<input type="submit" name="buscador" value="Buscar">
+			</form>
+		</div>	
+			
+		<?
+		
+		
+	if (isset($_POST['buscador']))
+	{		
+	// Tomamos el valor ingresado
+	$buscar = $_POST['palabra'];
+
+	// Si está vacío, lo informamos, sino realizamos la búsqueda
+	if(empty($buscar))
+	{
+	echo "No se ha ingresado una cadena a buscar";
+	}else{		
+	// Conexión a la base de datos y seleccion de registros
+	$link = mysql_connect("localhost", "root", "") or die ;
+	mysql_select_db("phototroxo", $link);
+
+	$sql = "SELECT * FROM imagen WHERE titulo like '%$buscar%' ORDER BY id DESC";
+	mysql_select_db("base_de_datos", $link);
+
+	$result = mysql_query($sql, $link);
+
+	// Tomamos el total de los resultados
+	
+
+	// Imprimimos los resultados
+	if ($row = mysql_fetch_array($result)){
+	echo "Resultados para: <b>$buscar</b>";
+	do {
+	?>
+	<p><b><a href= //no sé muy bien aqui como tengo que mostrar el resultado
+	<?
+	} while ($row = mysql_fetch_array($result));
+	echo "<p>Resultados: $total</p>";
+	} else {
+	// En caso de no encontrar resultados
+	echo "No se encontraron resultados para: <b>$buscar</b>";
+	}
+	}
+	}
+	?>
+	</div>
 		<!-- Pie de página -->
 		<?php
 		include ("piedepagina.php");
